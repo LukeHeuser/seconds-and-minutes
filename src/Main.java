ce7f701df89a6662bb0b1eab9f384b73754b87a0
Luke Heuser <103976088+LukeHeuser@users.noreply.github.com>
2024-09-04 20:55:59 -0400

    First Commit

--- src/Main.java
+++ src/Main.java
@@ -1,30 +1,34 @@
 public class Main {
     public static void main(String[] args) {
 
+        System.out.println( 59 % 60);
 
         // Test cases / Validation
-//        getDurationString(-756545);
-//        getDurationString(-45,45);
-//        getDurationString(45,-45);
+        getDurationString(-756545);
+        getDurationString(-45,45);
+        getDurationString(45,-45);
+        getDurationString(546,60);
 
+        //
         System.out.println(getDurationString(60));
-        System.out.println(getDurationString(45684));
+        System.out.println(getDurationString(130));
         System.out.println(getDurationString(546,50));
         System.out.println(getDurationString(645));
         System.out.println(getDurationString(500));
 
     }
 
-    public static String getDurationString(int seconds){
+
+    public static String getDurationString(int seconds){ // This method takes the seconds and finds the minutes, giving that information to the other Method.
         if (seconds < 0) {
             System.out.println("There can not be a negative amount of seconds.");
-            return "0";
+            return "";
         }
 
         int minutes = seconds / 60;
-        seconds = seconds - (minutes * 60);
+        int remainingSeconds = seconds % 60;
 
-        return getDurationString(minutes,seconds);
+        return getDurationString(minutes,remainingSeconds); //
     }
 
     public static String getDurationString(int minutes, int seconds){
@@ -33,7 +37,7 @@ public class Main {
             System.out.println("There can not be a negative amount of minutes.");
             return "0";
         }
-        if (seconds < 0 || seconds > 60) {
+        if (seconds < 0 || seconds > 59) {
             System.out.println("Seconds must be between 0 and 59.");
             return "0";
         }
@@ -41,7 +45,7 @@ public class Main {
         int hours = minutes / 60;
         minutes = minutes - (hours * 60);
 
-        return (hours + "h " + minutes + " m " + seconds + " s");
+        return (hours + "h " + minutes + "m " + seconds + "s");
     }
 
 }
